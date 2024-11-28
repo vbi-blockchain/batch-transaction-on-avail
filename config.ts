b05002bd935091b4b8c47a09608e6112841e104b
0xboji2004 👑 <126134422+0xBoji@users.noreply.github.com>
2024-11-28 14:51:02 +0700

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,14 @@
+export default {
+  seed: "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+  seed2: "burger wing ticket orchard unit simple harvest vote junk hungry cat frozen//Pich",
+  endpoint: "wss://goldberg.avail.tools/ws",
+  bridgeApi: "https://turing-bridge-api.fra.avail.so",
+  ethProviderUrl: "https://ethereum-sepolia-rpc.publicnode.com",
+  bridgeContractAddress: "0x967F7DdC4ec508462231849AE81eeaa68Ad01389",
+  data: "0",
+  appId: 0,
+  amount: 1,
+  recipient: "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
+  sessionKeys:
+    "0xcce44c3da975792242a278a90e1557ee2059ae14a6c6104a50045e13afdaea490028ae395391cba3e7aa5219802a04a0c1833b0814ed5bfae7e5b9c453a69bbedc69835386108accc1f191b82b40d92568b5e0863243cbe0351d36d5fc823b09187d3992202265cdce9d1b95481a402c9ca39fb041615ca71992d92066841534",
+}
